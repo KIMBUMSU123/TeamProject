3eecce402f41c802835fdeea4030085f2adeb573
KIMBUMSU123 <88920973+KIMBUMSU123@users.noreply.github.com>
2023-10-04 00:02:27 +0900

    swiper-wrapper 임시

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -5,33 +5,30 @@
 */
 //
 // Scripts
-// 
-var slide = new Swiper('.swiper', {
-    slidesPerView: '1', // 한 슬라이드에 보여줄 갯수
-    spaceBetween: 6, // 슬라이드 사이 여백
-    loop: false, // 슬라이드 반복 여부
-    pagination: true, // pager 여부
+//
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 20,
     autoplay: { // 자동 슬라이드 설정
         delay: 3000, // 시간 설정
         disableOnInteraction: false, // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
     },
-    navigation: { // 버튼 사용자 지정
+    // pagination: true, // pager 여부
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    },
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    breakpoints: { //반응형
-        // 화면의 넓이가 320px 이상일 때
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        // 화면의 넓이가 640px 이상일 때
-        640: {
-            slidesPerView: 4,
-            spaceBetween: 40
-        }
-    }
-})
+});
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -49,7 +46,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-    // Shrink the navbar 
+    // Shrink the navbar
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
